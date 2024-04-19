1f1e4c3cdc1ce989d57a906611f744c170eda8c6
yuzuki <y.yamahata.sys23@morijyobi.ac.jp>
2024-04-19 11:04:36 +0900

    quiz.jsの修正

--- quiz.js
+++ quiz.js
@@ -1,55 +1,44 @@
-"use userstrict";
-
-/* ==============================================
- 基本データ
- ================================================ */
-
-//  地理のクイズデータ
+`use strict`;
+//0.基本データ
+//地理のクイズデータ
 const data = [
     {
-        question: "日本で一番面積の大きい都道府県は？",
-        answers: ["北海道", "京都", "青森", "岩手",],
-        correct: "北海道"
+        quetion: "日本で一番面積の大きい都道府県は？",
+        answers: ["青森県", "東京都", "北海道", "福岡県"],
+        correct: "青森県"
     },
     {
-        question: "日本で一番人口の多い都道府県は？",
-        answers: ["北海道", "東京都", "青森", "岩手",],
-        correct: "東京都"
+        quetion: "日本で一番人口の多い都道府県は？",
+        answers: ["福岡県", "青森県", "東京都", "長野県"],
+        correct: "青森県"
     },
     {
-        question: "日本で一番人口密度の高い都道府県は？",
-        answers: ["北海道", "東京都", "青森", "岩手",],
-        correct: "東京都"
+        quetion: "日本で一番人口密度の高い都道府県は？",
+        answers: ["東京都", "茨城県", "福島県", "青森県"],
+        correct: "青森県"
     }
 ]
-
 // 出題する問題数
 const QUESTION_LENGTH = 3;
-// 解答時間(ms)
+// 出題する問題数
 const ANSWER_TIME_MS = 10000;
-// 
+// インターバル時間
 const INTERVAL_TIME_MS = 10;
 
+// 解答開始時間
 let startTime = null;
 // インターバルID
-let IntervalID = null;
-
-// 解答中の経過時間
+let intervalId = null;
+// 回答中の経過時間
 let elapsedTime = 0;
-
-// 出題する問題データ
-let questions = [];
+//出題する問題データ
+// let questions = data.slice(0, QUESTION_LENGTH);
+let questions = getRandamQuestions();
 // 出題する問題のインデックス
 let questionIndex = 0;
-// 正解数
+//正解数
 let correctCount = 0;
-
-
-
-/* ==============================================
- 要素一覧
- ================================================ */
-
+//1.要素一覧
 const startPage = document.getElementById("startPage");
 const questionPage = document.getElementById("questionPage");
 const resultPage = document.getElementById("resultPage");
@@ -58,227 +47,203 @@ const startButton = document.getElementById("startButton");
 
 const questionNumber = document.getElementById("questionNumber");
 const questionText = document.getElementById("questionText");
-const optionButton = document.querySelectorAll("#questionPage button");
+const optionButtons = document.querySelectorAll("#questionPage button");
 const questionProgress = document.getElementById("questionProgress");
-
 const resultMessage = document.getElementById("resultMessage");
 const backButton = document.getElementById("backButton");
 
 const dialog = document.getElementById("dialog");
 const questionResult = document.getElementById("questionResult");
 const nextButton = document.getElementById("nextButton");
-
-
-
-/* ==============================================
- 処理
- ================================================ */
-
+//2.処理
 startButton.addEventListener("click", clickstartButton);
-
-for (let i = 0; i < optionButton.length; i++){
-    optionButton[i].addEventListener("click", clickoptionButton);
-}
-// optionButton.forEach((button) => {
-//     button.addEventListener("click", clickoptionButton)
-// });
-
+optionButtons.forEach((button) => {
+    button.addEventListener("click", clickOptionButton);
+});
 nextButton.addEventListener("click", clickNextButton);
 
 backButton.addEventListener("click", clickBackButton);
-
-/* ==============================================
- 関数一覧
- ================================================ */
-
-function questionIntervalOver() {
-    // 
-    questionResult.innerText = "×";
-    // 
-    
-    if (isQuestion()) {
+//関数一覧
+function questionTimeOver() {
+    // 時間切れの場合は不正解とする
+    questionResult.innerText = "✖";
+    // ダイアログの
+    if (isQuestionEnd()) {
         nextButton.innerText = "結果を見る";
+
     } else {
         nextButton.innerText = "次の問題へ";
     }
-    // 
     dialog.showModal();
-    
 }
-
 function startProgress() {
-    // 開始時間(タイムスタンプ)を取得する
+    // 開始時間（タイムスタンプ）を取得する
     startTime = Date.now();
     // インターバルを開始する
-    IntervalID = setInterval(() => {
-        const currectTime = Date.now();
-        // 現在時刻(タイムスタンプ)を取得する
-        const progress = ((currectTime - startTime) / ANSWER_TIME_MS) * 100;
-        // progressバーに経過時間を反映(表示)する
-        questionProgress.value = progress;
-        // 経過時間が解答時間を超えた場合、インターバルを停止する
-        if (startTime + ANSWER_TIME_MS <= currectTime) {
+    intervalId = setInterval(() => {
+        const currentTime = Date.now();
+        // 経過時間計測する
+        const Progress = ((currentTime - startTime) / ANSWER_TIME_MS) * 100;
+        // progressバーに経過時間を反映する（表示）
+        questionProgress.value = Progress;
+        // 経過時間が街道時間を超えた場合、インターバルを停止する
+        if (startTime + ANSWER_TIME_MS <= currentTime) {
             stopProgress();
-            questionIntervalOver();
+            questionTimeOver();
             return;
         }
-        // 経過時間を更新(加算)する
+        // 経過時間を更新加算する
         elapsedTime += INTERVAL_TIME_MS;
     }, INTERVAL_TIME_MS);
+    // // インターバルタイムを取得する
+    // intervalId = setInterval(() => {
+    //     // 経過時間計測する
+    //     const Progress = (elapsedTime / ANSWER_TIME_MS) * 100;
+    //     // progressバーに経過時間を反映する（表示）
+    //     questionProgress.value = Progress;
+    //     // 経過時間が街道時間を超えた場合、インターバルを停止する
+    //     if (ANSWER_TIME_MS <= elapsedTime) {
+    //         stopProgress();
+    //         questionTimeOver();
+    //         return;
+    //     }
+    //     // 経過時間を更新加算する
+    //     elapsedTime += INTERVAL_TIME_MS;
+    // }, INTERVAL_TIME_MS);
 }
-
 function stopProgress() {
     // インターバルを停止する
-    if (IntervalID !== null) {
-        clearInterval(IntervalID);
-        IntervalID = null;
+    if (intervalId !== null) {
+        clearInterval(intervalId);
+        intervalId = null;
     }
 }
-
-
 function reset() {
     // 出題する問題をランダムに取得する
-    questions = getRandomQuestion();
-    // 出題する問題のインデックスの初期化
+    questions = getRandamQuestions();
+    // 出題する問題のインデクスを初期化する
     questionIndex = 0;
-    // 正解数の初期化
+    // 正解数を初期化する
     correctCount = 0;
     // インターバルIDを初期化する
-    IntervalID = null;
-    // 解答中の経過時間を初期化する
+    intervalId = null;
+    // 解凍中の経過時間を初期化する
     elapsedTime = 0;
+    // 解答時間を初期化する
     startTime = null;
-    // ボタンを有効か
-    for (let i = 0; i < optionButton.length; i++) {
-        optionButton[i].removeAttribute("disabled");
+    // ボタンを有効化する
+    for (let i = 0; i < optionButtons.length; i++) {
+        optionButtons[i].removeAttribute("disabled");
     }
 }
-
-function isQuestion() {
-    // 問題数が最後かどうか
+function isQuestionEnd() {
+    //問題が最後かどうかを判定する
     return questionIndex + 1 === QUESTION_LENGTH;
 }
-
-function getRandomQuestion() {
-    // 出題する問題数のインデックスリスト
-    const questionIndexlist = [];
-    while (questionIndexlist.length !== QUESTION_LENGTH) {
-        // 出題する問題のインデックスをランダムに生成する
+function getRandamQuestions() {
+    //出題する問題のインデックスリスト
+    const questionIndexList = [];
+    while (questionIndexList.length !== QUESTION_LENGTH) {
+        //出題する問題のインデックスをランダムに生成する
         const index = Math.floor(Math.random() * data.length);
-        // インデックスリストに含まれ邸内場合、インデックスリストに追加する
-        if (!questionIndexlist.includes(index)) {
-            questionIndexlist.push(index);
+        // インデックスに含まれていない場合、インデックスリストに追加する
+        if (!questionIndexList.includes(index)) {
+            questionIndexList.push(index);
         }
     }
     // 出題する問題リストを取得する
-    const questionlist = questionIndexlist.map((index) => data[index]);
-    return questionlist;
+    const questionList = questionIndexList.map((index) => data[index]);
+    return questionList
 }
-
-function setQestion() {
-    // 問題を取得する
+function setResult() {
+    //正解率を計算する
+    const accuracy = Math.floor((correctCount / QUESTION_LENGTH) * 100);
+    //正解率を表示する
+    resultMessage.innerText = `正解率：${accuracy}%`;
+}
+function setQuestion() {
+    //問題を取得する
     const question = questions[questionIndex];
-    // 問題番号を表示する
+    //問題番号を表示する
     questionNumber.innerText = `第 ${questionIndex + 1} 問`;
-    // 問題文を表示する
-    questionText.innerText = question.question;
-    // 選択肢を表示する
-    for (let i = 0; i < optionButton.length; i++) {
-        optionButton[i].innerText = question.answers[i];
+    questionText.innerText = question.quetion;
+    //選択肢を表示する
+    for (let i = 0; i < optionButtons.length; i++) {
+        optionButtons[i].innerText = question.answers[i];
     }
 }
-
-/*===============================================
- イベント関連の関数一覧
- ================================================ */
-
-function setResult() {
-    // 正解率
-    const accuracy = Math.floor((correctCount / QUESTION_LENGTH) * 100);
-    // 正解率を表示する
-    resultMessage.innerText = `正解率: ${accuracy}%`
-}
-
-function clickoptionButton(event) {
-    // 解答中の経過時間を停止する
+//3.イベント関連の関数一覧
+function clickOptionButton(event) {
+    // 解答の経過時間を止める
     stopProgress();
-    // すべての選択肢を無効化する
-    optionButton.forEach((button) => {
+    // すべての選択肢を無効かする
+    optionButtons.forEach((button) => {
         button.disabled = true;
-    });
-
-    // 選択肢のテキストを取得する
+    }
+    );
+    //解答処理を実施する
     const optionText = event.target.innerText;
-    // 正解のテキストを取得する
+    //正解のテキストを取得する
     const correctText = questions[questionIndex].correct;
-
     if (optionText === correctText) {
         correctCount++;
-        questionResult.innerText = "〇";
+        questionResult.innerText = "⭕";
     } else {
-        questionResult.innerText = "×";
+        questionResult.innerText = "✖";
     }
-
-    // 最後の問題かどうかを判定する 
-    if (isQuestion()) {
+    if (isQuestionEnd()) {
         nextButton.innerText = "結果を見る";
     } else {
         nextButton.innerText = "次の問題へ";
     }
-
-    // ダイアログ
+    // dialog.showModal();
     dialog.showModal();
 }
-
 function clickstartButton() {
     // クイズをリセットする
     reset();
-    // 問題画面に問題を設定する
-    setQestion();
-    // 解答の計測を設定する
+    //問題画面に問題を設定する
+    setQuestion();
+    // 解答を計測開始
     startProgress();
-    // スタート画面を非表示にする
+    //スタート画面を非表示にする
     startPage.classList.add("hidden");
-    // 問題画面を表示する
+    //問題画面を表示する
     questionPage.classList.remove("hidden");
-    // 結果画面を非表示にする
+    //結果画面を非表示にする
     resultPage.classList.add("hidden");
 }
-
 function clickNextButton() {
-    if (isQuestion()) {
-        // 結果画面に正解率を設定する
+    if (isQuestionEnd()) {
+        // 正解率を設定する
         setResult();
-        // 正解・不正解のダイアログを閉じる
+        // ダイアログを閉じる
         dialog.close();
-        // 問題画面を非表示する
+        //スタート画面を非表示にする
+        startPage.classList.add("hidden");
+        //問題画面を表示する
         questionPage.classList.add("hidden");
-        // 結果画面を表示にする
+        //結果画面を非表示にする
         resultPage.classList.remove("hidden");
     } else {
-        optionButton.forEach((button) => {
-            button.disabled = false;
-        });
-        // 問題のインデックスを更新する
         questionIndex++;
-        // 問題を設定する
-        setQestion();
-        // IDの初期化
-        IntervalID = null;
-        // 経過時間の初期化
+        setQuestion();
+        // インターバルを初期化する
+        intervalId = null;
+        // 解答中の経過時間を
         elapsedTime = 0;
-        // 正解・不正解のダイアログを閉じる
+        for (let i = 0; i < optionButtons.length; i++) {
+            optionButtons[i].removeAttribute("disabled");
+        }
         dialog.close();
-        // 計測開始
-        startProgress
+        startProgress();
     }
 }
-
 function clickBackButton() {
-    // スタート画面を表示にする
+    //スタート画面を表示にする
     startPage.classList.remove("hidden");
-    // 問題画面を非表示する
+    //問題画面を非表示する
     questionPage.classList.add("hidden");
-    // 結果画面を非表示にする
+    //結果画面を非表示にする
     resultPage.classList.add("hidden");
 }
